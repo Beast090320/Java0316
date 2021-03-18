e1524addf32bf6d797aac0c2dbccaba52735d4cb
MB-study <MB-study@DESKTOP-KVQTFGR.lan>
2021-03-18 20:59:31 +0800

    IdontNo

---
+++ src/main/java/com/ocp/day02/Student.java
@@ -0,0 +1,19 @@
+package com.ocp.day02;
+
+public class Student {
+    String name;
+    int age;
+    BMI bmi;
+    
+    
+    Student(String name, int age, double height, double weight) {
+        this.name = name;
+        this.age = age;
+        this.bmi = new BMI(name, height, weight);
+    }
+    
+    void print(){
+        System.out.printf("姓名:%s 年齡:%d\n",name ,age );
+        bmi.print();
+    }
+}
