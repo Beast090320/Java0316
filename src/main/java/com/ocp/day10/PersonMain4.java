c251c9be2e0e573684d631dc567a1725d502e299
MB-study <MB-study@DESKTOP-KVQTFGR.lan>
2021-04-08 21:46:19 +0800

    IdontNo

--- src/main/java/com/ocp/day10/PersonMain4.java
+++ src/main/java/com/ocp/day10/PersonMain4.java
@@ -1,7 +1,11 @@
 package com.ocp.day10;
 
+import java.util.DoubleSummaryStatistics;
+import java.util.IntSummaryStatistics;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
+import sun.security.action.GetBooleanAction;
 
 public class PersonMain4 {
 
@@ -18,10 +22,29 @@ public class PersonMain4 {
             }
         };
         // 請印出所有人的 bmi 的資料
-        Function<Person, Double> getBMI = p -> p.getWeight() / Math.pow(p.getHeight()/100, 2);
+        Function<Person, Double> getBMI = p -> {
+                double bmi = p.getWeight() / Math.pow(p.getHeight()/100, 2);
+                return Math.round(bmi * 100) / 100.0;
+        };
         Stream.of(persons)
                 .flatMap(p -> Stream.of(p))
                 .mapToDouble(getBMI::apply)
                 .forEach(System.out::println); // value -> System.out.println(value)
+        // 請問哪一個 BMI 最大?
+        double maxBmi = Stream.of(persons)
+                .flatMap(p -> Stream.of(p))
+                .mapToDouble(getBMI::apply)
+                .summaryStatistics()
+                .getMax();
+        
+        Consumer<Person> printPerson = p -> {
+            System.out.printf("%s %d %.1f %.2f\n",
+                    p.getName(),p.getAge(),p.getHeight(),p.getWeight(),getBMI.apply(p));
+        };
+        
+        Stream.of(persons)
+                .flatMap(p -> Stream.of(p))
+                .filter(p -> getBMI.apply(p) == maxBmi)
+                .forEach(printPerson);
     }
 }
