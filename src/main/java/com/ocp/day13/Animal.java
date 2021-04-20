f5aa5ccee07dc88284cb6db7369230909d646747
MB-study <MB-study@DESKTOP-KVQTFGR.lan>
2021-04-20 21:23:04 +0800

    IdontNo

--- src/main/java/com/ocp/day13/Animal.java
+++ src/main/java/com/ocp/day13/Animal.java
@@ -1,8 +1,7 @@
 package com.ocp.day13;
 
 public interface Animal {
-    int legs = 4; //初始值 
-    void eat();   //規格 方法裡邏輯不能寫
-    void move();  //規格內屬性是不可改變的 
-
+    int legs = 4;
+    void eat();
+    void move();
 }
