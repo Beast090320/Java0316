366dafc697ab3ed238c9bf6da760a267db71d69d
MB-study <MB-study@DESKTOP-KVQTFGR.lan>
2021-05-11 20:47:28 +0800

    IdontNo

---
+++ src/main/java/com/lab/order_system/ServiceCenter.java
@@ -0,0 +1,17 @@
+package com.lab.order_system;
+
+import java.util.Set;
+
+public class ServiceCenter {
+    private Set<Food> catalog = new DataCenter().getCatalog();
+    // 檢視 Catalog
+    public void printCatalog() {
+        System.out.println("訂單系統");
+        System.out.println("--------------------------------------------");
+        for(Food food : catalog) {
+            System.out.printf("編號: %2d 品名: %s 尺寸: %c 價格: %-3d\n",
+                    food.getNo(), food.getName(), food.getSize(), food.getPrice());
+        }
+        System.out.println("--------------------------------------------");
+    }
+}
