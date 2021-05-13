804d7b01c272d46e6c441003c7615b354ed39994
MB-study <MB-study@DESKTOP-KVQTFGR.lan>
2021-05-13 19:52:06 +0800

    IdontNo

--- src/test/java/test_cdc/test_Util.java
+++ src/test/java/test_cdc/test_Util.java
@@ -1,5 +1,6 @@
 package test_cdc;
 
+import com.lab.cdc.CDC;
 import com.lab.cdc.Util;
 import org.junit.Test;
 
@@ -10,4 +11,11 @@ public class test_Util {
         String json = Util.getJson();
         System.out.println(json);
     }
+
+    @Test
+    public void test_getCdcs() throws Exception {
+        CDC[] cdcs = Util.getCdcs();
+        System.out.println(cdcs.length);
+    }
+
 }
