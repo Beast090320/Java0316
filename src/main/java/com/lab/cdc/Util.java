190d5e464511341936638bc82962d5df825112b6
MB-study <MB-study@DESKTOP-KVQTFGR.lan>
2021-05-13 19:41:22 +0800

    IdontNo

---
+++ src/main/java/com/lab/cdc/Util.java
@@ -0,0 +1,25 @@
+package com.lab.cdc;
+
+// 工具程式
+
+
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+
+
+public class Util {
+
+    public static String getJson() throws Exception {
+        // 資料來源 
+        String path = "https://www.cdc.gov.tw/TravelEpidemic/ExportJSON";
+        // 建立 URL 物件
+        URL url = new URL(path);
+        // 取得 json 文字資料
+        String json = new Scanner(new InputStreamReader(url.openStream(), "UTF-8"))
+                .useDelimiter("\\A")
+                .next();
+        
+         return (json.charAt(0) == '?')?json.substring(1):json;
+    }
+}
